b66c2086351e56fe03521cd958e08d0ba1873a6f
yandsa <yandsa@yonyou.com>
2017-09-13 17:16:41 +0800

    add

--- src/main/java/org/cboard/security/service/LocalSecurityFilter.java
+++ src/main/java/org/cboard/security/service/LocalSecurityFilter.java
@@ -27,44 +27,47 @@ import com.google.common.cache.LoadingCache;
  */
 public class LocalSecurityFilter implements Filter {
 
-    private static LoadingCache<String, String> sidCache = CacheBuilder.newBuilder().expireAfterAccess(1, TimeUnit.MINUTES).build(new CacheLoader<String, String>() {
-        @Override
-        public String load(String key) throws Exception {
-            return null;
-        }
-    });
+	private static LoadingCache<String, String> sidCache = CacheBuilder.newBuilder()
+			.expireAfterAccess(1, TimeUnit.MINUTES).build(new CacheLoader<String, String>() {
+				@Override
+				public String load(String key) throws Exception {
+					return null;
+				}
+			});
 
-    public static void put(String sid, String uid) {
-        sidCache.put(sid, uid);
-    }
+	public static void put(String sid, String uid) {
+		sidCache.put(sid, uid);
+	}
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
 
-    }
+	}
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-    	if ("/render.html".equals(((HttpServletRequest) servletRequest).getServletPath())) {
-            User user = new User("shareUser", "", new ArrayList<>());
-            user.setUserId("1");
-            SecurityContext context = SecurityContextHolder.getContext();
-            context.setAuthentication(new ShareAuthenticationToken(user));
-            ((HttpServletRequest) servletRequest).getSession().setAttribute("SPRING_SECURITY_CONTEXT", context);
-        }else{
-        	SecurityContext context = (SecurityContext) ((HttpServletRequest) servletRequest).getSession().getAttribute("SPRING_SECURITY_CONTEXT");
-        	if(context!=null){
-        		Authentication authentication = context.getAuthentication();
-        		if(authentication!=null&&("shareUser").equals(authentication.getName())){
-        			((HttpServletRequest) servletRequest).getSession().setAttribute("SPRING_SECURITY_CONTEXT",null);
-        		}
-        	}
-        }
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+			throws IOException, ServletException {
+		if ("/render.html".equals(((HttpServletRequest) servletRequest).getServletPath())) {
+			User user = new User("shareUser", "", new ArrayList<>());
+			user.setUserId("1");
+			SecurityContext context = SecurityContextHolder.getContext();
+			context.setAuthentication(new ShareAuthenticationToken(user));
+			((HttpServletRequest) servletRequest).getSession().setAttribute("SPRING_SECURITY_CONTEXT", context);
+		} else {
+			SecurityContext context = (SecurityContext) ((HttpServletRequest) servletRequest).getSession()
+					.getAttribute("SPRING_SECURITY_CONTEXT");
+			if (context != null) {
+				Authentication authentication = context.getAuthentication();
+				if (authentication != null && ("shareUser").equals(authentication.getName())) {
+					((HttpServletRequest) servletRequest).getSession().setAttribute("SPRING_SECURITY_CONTEXT", null);
+				}
+			}
+		}
+		filterChain.doFilter(servletRequest, servletResponse);
+	}
 
-    @Override
-    public void destroy() {
+	@Override
+	public void destroy() {
 
-    }
+	}
 }
